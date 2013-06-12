3984579f10426f0d90111e9817cf4671b4489f3d
Harrison Kelly <harrison@verifiedstudios.com>
2013-06-12 19:35:20 -0400

    Getting the project working with some sample files.

--- mock_project/settings.py
+++ mock_project/settings.py
@@ -1,6 +1,6 @@
 import os
 
-# Traverse up to the directory where manage.py
+# Where's the first-level project directory, where this file resides?
 SITE_ROOT = os.path.abspath(os.path.join(__file__, os.pardir))
 
 DEBUG = True
@@ -69,12 +69,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media') + '/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
